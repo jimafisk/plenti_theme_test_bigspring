a0638af4567c13b1f514c2b9004261779152e8ec
jimafisk <jimafisk@gmail.com>
2023-01-06 14:12:06 -0500

    Update bigspring.

--- themes/bigspring.git/layouts/global/footer.svelte
+++ themes/bigspring.git/layouts/global/footer.svelte
@@ -5,9 +5,9 @@
         
         <div class="h3 mb-4">Company</div>
         <ul class="list-unstyled footer-list">
-          <li><a href="/">About Us</a></li>
-          <li><a href="/pricing">Pricing</a></li>
-          <li><a href="/">Quick Start</a></li>
+          <li><a href=".">About Us</a></li>
+          <li><a href="pricing">Pricing</a></li>
+          <li><a href=".">Quick Start</a></li>
         </ul>
         
       </div>
@@ -15,9 +15,9 @@
         
         <div class="h3 mb-4">Product</div>
         <ul class="list-unstyled footer-list">
-          <li><a href="/">Features</a></li>
-          <li><a href="/">Platform</a></li>
-          <li><a href="/pricing">Pricing</a></li>
+          <li><a href=".">Features</a></li>
+          <li><a href=".">Platform</a></li>
+          <li><a href="pricing">Pricing</a></li>
         </ul>
         
       </div>
@@ -25,21 +25,21 @@
         
         <div class="h3 mb-4">Support</div>
         <ul class="list-unstyled footer-list">
-          <li><a href="/faq">FAQ</a></li>
-          <li><a href="/privacy-policy">Privacy Policy</a></li>
-          <li><a href="/terms-conditions">Terms &amp; Conditions</a></li>
+          <li><a href="faq">FAQ</a></li>
+          <li><a href="privacy-policy">Privacy Policy</a></li>
+          <li><a href="terms-conditions">Terms &amp; Conditions</a></li>
         </ul>
         
       </div>
       <div class="col-md-3 col-sm-6 mb-5">
-        <a class="d-block mb-3" href="/"><img width="200px" class="img-fluid" src="/assets/logo.png" alt="Bigspring Hugo"></a>
+        <a class="d-block mb-3" href="."><img width="200px" class="img-fluid" src="assets/logo.png" alt="Bigspring Hugo"></a>
         <p class="mb-4">Lorem ipsum dolor sit amet, consectetur elit. Consjat tristique eget amet, tempus eu at cttur.</p>
         
         <ul class="list-inline social-icons">
-          <li class="list-inline-item"><a href="https://www.facebook.com/jantcu/"><i class="ti-facebook"></i></a></li>
-          <li class="list-inline-item"><a href="https://twitter.com/plentico"><i class="ti-twitter-alt"></i></a></li>
-          <li class="list-inline-item"><a href="/"><i class="ti-skype"></i></a></li>
-          <li class="list-inline-item"><a href="https://www.linkedin.com/company/jantcu"><i class="ti-linkedin"></i></a></li>
+          <li class="list-inline-item"><a href="https://www.facebook.com/jantcu/" aria-label="Facebook"><i class="ti-facebook"></i></a></li>
+          <li class="list-inline-item"><a href="https://twitter.com/plentico" aria-label="Twitter"><i class="ti-twitter-alt"></i></a></li>
+          <li class="list-inline-item"><a href="." aria-label="Skype"><i class="ti-skype"></i></a></li>
+          <li class="list-inline-item"><a href="https://www.linkedin.com/company/jantcu" aria-label="LinkedIn"><i class="ti-linkedin"></i></a></li>
         </ul>        
       </div>
     </div>
